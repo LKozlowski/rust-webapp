c5d2f6de4cf4a292dd8f1245056e322ac05106aa
Lukasz Kozlowski <innyadresemail@gmail.com>
2021-09-22 16:26:49 +0200

    show current datetime in main route

--- webapp/src/main.rs
+++ webapp/src/main.rs
@@ -1,19 +1,21 @@
-use warp::{Reply, Rejection, Filter};
-
+use warp::{Filter, Rejection, Reply};
 
 type Result<T> = std::result::Result<T, Rejection>;
 
 #[tokio::main]
 async fn main() {
     let healthz_route = warp::path!("healthz").and_then(healtz_handler);
-    let routes = healthz_route.with(warp::cors().allow_any_origin());
-
+    let hello_world = warp::path::end().map(|| {
+        format!(
+            "Hello, World!\n\nCurrent Time: {}",
+            chrono::offset::Utc::now()
+        )
+    });
+    let routes = warp::get().and(hello_world.or(healthz_route));
     println!("started server at localhost:8000");
-    warp::serve(routes).run(([0,0,0,0], 8000)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
-
 
 async fn healtz_handler() -> Result<impl Reply> {
     Ok("OK")
 }
-
